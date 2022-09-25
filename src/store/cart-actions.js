94f0a0fad80900e73cf31734a8dbc75cdf06cd42
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-25 19:47:51 +0200

    Create action creator thunk

---
+++ src/store/cart-actions.js
@@ -0,0 +1,43 @@
+import { uiActions } from "./ui-slice";
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-redux-cart-58b65-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        { method: "PUT", body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong! Sending cart data failed");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
